0c80d26f2bd23e71964bd0337e2082742f5ba0bc
Алексей Губич <a.v.gubich@proton.me>
2023-05-16 22:19:23 +0300

    add install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+dnf update && dnf upgrade -y
+dnf install docker -y
+systemctl start docker
+
+
+git clone https://github.com/AVGubich/test_stress.git
+cd /root/test_stress/
+
+for (( i=1; i <= 10; i++ ))
+do
+docker build .
+done
